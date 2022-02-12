f2c13e687c531fbd31075af58c489854a2f8e88b
catkane-doodles <tanpeikai@hotmail.com>
2022-02-12 21:20:27 +0000

    styling

--- cextension/after.js
+++ cextension/after.js
@@ -1,47 +1,35 @@
 // this code will be executed after page load
-(function() {
-  console.log('after.js executed');
+(function () {
+  console.log("after.js executed");
 
   const modal = document.createElement("div");
-  modal.className = "box"
-  modal.setAttribute(
-    "style",`
-    right: 10px;
-    top: 10px;
-    bottom: 10px;
-    height: 90%;
-    position:absolute;
-    background-color: white;
-    border-radius: 6px;
-    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
-    color: #4a4a4a;
-    display: block;
-    padding: 1.25rem;
-    `
-  );
-  modal.setAttribute("id","highlightDialog")
-  modal.innerHTML = `<iframe id="popup-content"; style="height:100%;"></iframe>
-  <div style="position:absolute; top:0px; left:5px;">
-  </div>`;
+  modal.className = "box";
+  modal.setAttribute("id", "highlightDialog");
+  modal.innerHTML = `
+  <label class='switch'> <input type='checkbox' checked> <span class='slider round'></span></label>
+  <input type='text' id='fname' name='fname'>
+  <button type='button' id='submit' name='submit'>Submit</button>
+  <div class=modalFooter>
+  <strong>Give Me The Source</strong> <br>by RushHour2022
+  </div>
+  `;
+
   document.body.appendChild(modal);
-  modal.style.display = 'none'
+  modal.style.display = "none";
 })();
 
-
 function mouseUp() {
-    if (window.getSelection().toString() == ""){
-      console.log("there is no selected text")
+  if (window.getSelection().toString() == "") {
+    console.log("there is no selected text");
+  } else {
+    console.log(window.getSelection().toString());
+    if (event.shiftKey) {
+      console.log("Shift held");
+      openModal();
+    } else {
+      console.log("Shift not held");
     }
-    else {
-      console.log(window.getSelection().toString())
-      if (event.shiftKey) {
-        console.log("Shift held")
-        openModal();
-      } else {
-        console.log("Shift not held")
-      }
-    }
-    
+  }
 }
 
 // function openModal(){
@@ -70,17 +58,17 @@ function mouseUp() {
 
 //   const iframe = document.getElementById("popup-content");
 //   iframe.frameBorder = 0;
-//   if(iframe.contentDocument) { 
-//     doc = iframe.contentDocument; 
+//   if(iframe.contentDocument) {
+//     doc = iframe.contentDocument;
 //   } else {
-//     doc = iframe.contentWindow.document; 
+//     doc = iframe.contentWindow.document;
 //   }
 
 //   var requestOptions = {
 //     method: 'GET',
 //     redirect: 'follow'
 //   };
-  
+
 //   fetch("https://code.peikai.pii.at/iframe?text="+window.getSelection().toString(), requestOptions)
 //     .then(response => {
 //           // dialog.showModal()
@@ -95,7 +83,7 @@ function mouseUp() {
 //     }
 //     )
 //     .catch(error => console.log('error', error));
-// }  
+// }
 
 // const closeModal = () => {
 //   modal = document.getElementById("highlightDialog")
