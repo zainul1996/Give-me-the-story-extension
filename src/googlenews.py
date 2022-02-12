7c6d024dbf742cdbcc15db4bd3d3f42644f4f91b
Zainul1996 <zainul1996@gmail.com>
2022-02-12 22:37:03 +0800

    updated group by year

--- src/googlenews.py
+++ src/googlenews.py
@@ -9,19 +9,24 @@ reliable_sources_href = [
     "https://www.bloomberg.com",
 ]
 
+unreliable_sources_href = [
+    "https://mothership.sg"
+]
+
 
 def filterReliableNews(news):
     return [item for item in news if item["publisher"]["href"] in reliable_sources_href]
 
 
-def filterMatched(news, keywords):
+def filterMatched(news, keywords, matchThreshold):
     for item in news:
         item["score"] = 0
         for keyword in keywords:
-            if keyword in item["title"]:
+            if keyword.upper() in item["title"].upper():
                 item["score"] += 1
-
-    return [item for item in news if item["score"] > 0]
+    
+    news = [item for item in news if item["score"] >= matchThreshold*len(keywords)]
+    return news
 
 
 def sortByScore(news):
@@ -35,38 +40,32 @@ def getNews(keywords=[]):
         news = google_news.get_news(search_keyword)
 
         return news
-
-
-def getRootNews(news):
-    # parse every items 'published date' string in 'Fri, 12 Nov 2021 08:00:00 GMT' format to datetime object
+def addParsedDate(news):
     for item in news:
-        item["formatted published date"] = datetime.datetime.strptime(
-            item["published date"], "%a, %d %b %Y %H:%M:%S %Z"
-        )
+        item["formatted published date"] = datetime.datetime.strptime(item["published date"], "%a, %d %b %Y %H:%M:%S %Z").timestamp()
+    return news
 
-    # sort news by 'published date'
+def groupByYear(news):
     news = sorted(news, key=lambda k: k["formatted published date"], reverse=False)
+    grouped_news = {}
+    for item in news:
+        date = datetime.datetime.fromtimestamp(item["formatted published date"])
+        year = date.year
+        if year not in grouped_news:
+            grouped_news[year] = []
+        grouped_news[year].append(item)
+    return grouped_news
 
-    return news[0]
 
-
-def run(keywords=[]):
+def run(mode, keywords=[]):
+    matchThreshold = 0.5
     if len(keywords) > 0:
-        search_keyword = " ".join(str(x) for x in keywords)
-        google_news = GNews()
-        news = google_news.get_news(search_keyword)
-
-        for item in news:
-            item["score"] = 0
-            for keyword in keywords:
-                if keyword in item["title"]:
-                    item["score"] += 1
-
         news = getNews(keywords)
-        news = filterMatched(news, keywords)
+        news = filterMatched(news, keywords, matchThreshold)
         news = sortByScore(news)
-        news = filterReliableNews(news)
-
+        news = addParsedDate(news)
+        # news = filterReliableNews(news)
+        news = groupByYear(news)
         return news
     else:
         return "No keywords provided"
--- test/test_news.py
+++ test/test_news.py
@@ -1,18 +1,15 @@
 import pytest
+import json
 
 from src import googlenews
 
 
 def test_googlenews():
-    keywords = ["covid", "infection", "Singapore"]
-    news = googlenews.run(keywords)
-
+    keywords = ["Iris", "Koh", "healing", "the", "divide"]
+    news = googlenews.run("news", keywords)
+    # write to file
+    with open("test_news.json", "w") as f:
+        json.dump(news, f)
     assert len(news) > 0 or news == "No keywords provided"
 
 
-def test_root_news():
-    keywords = ["covid", "infection", "Singapore"]
-    news = googlenews.run(keywords)
-    rootNews = googlenews.getRootNews(news)
-
-    assert isinstance(rootNews, dict)
