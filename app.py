de99f4a28ad821e47a030d755aabafd44853729e
catkane-doodles <tanpeikai@hotmail.com>
2022-02-12 15:59:09 +0000

    mvp for gmts

--- app.py
+++ app.py
@@ -63,12 +63,7 @@ def site():
     text = request.args.get("text")
     keywords = keyextraction.extract_key(text)
 
-    print(keywords)
-
     articles = googlenews.run("news", keywords)
-
-    print(articles)
-    print("hello")
 
     return render_template("index.html", text=text, articles=articles)
 
@@ -76,12 +71,6 @@ def site():
 @app.route("/iframe")
 def iframe():
     text = request.args.get("text")
-    keywords = keyextraction.extract_key(text)
-    # articles = googlenews.run(keywords)
-
-    # if (articles[0][0] != None):
-    #     return articles[0][0]['summary']
-
     dictionary_definition = definition.dictionary_extract(text)
     if (dictionary_definition != None):
         return dictionary_definition
