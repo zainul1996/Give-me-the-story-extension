40046b993a67f02419550dcb82cca4317912cbc9
Kok Fong <kokfong.lim@uparcel.sg>
2022-02-13 01:37:58 +0800

    updated popup box

--- cextension/after.js
+++ cextension/after.js
@@ -5,13 +5,19 @@
   const modal = document.createElement("div");
   modal.className = "box"
   modal.setAttribute(
-  "style",`
-  height:450px;
-  border: none;
-  top:150px;
-  border-radius:20px;
-  background-color:white;
-  `
+    "style",`
+    right: 10px;
+    top: 10px;
+    bottom: 10px;
+    height: 90%;
+    position:absolute;
+    background-color: white;
+    border-radius: 6px;
+    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+    color: #4a4a4a;
+    display: block;
+    padding: 1.25rem;
+    `
   );
   modal.setAttribute("id","highlightDialog")
   modal.innerHTML = `<iframe id="popup-content"; style="height:100%;"></iframe>
@@ -38,108 +44,66 @@ function mouseUp() {
     
 }
 
-function openModal(){
-  const dialog = document.getElementById("highlightDialog");
-  s = window.getSelection();
-  oRange = s.getRangeAt(0); //get the text range
-  oRect = oRange.getBoundingClientRect();
-  console.log(oRect)
-  console.log(oRect['x'])
-  console.log(oRect['y'])
-  dialog.setAttribute(
-    "style",`
-    left:`+oRect['x']+`px;
-    top:`+(oRect['y']+30)+`px;
-    position:absolute;
-    background-color: white;
-    border-radius: 6px;
-    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
-    color: #4a4a4a;
-    display: block;
-    padding: 1.25rem;
-    `
-  );
+// function openModal(){
+//   const dialog = document.getElementById("highlightDialog");
+//   // s = window.getSelection();
+//   // oRange = s.getRangeAt(0); //get the text range
+//   // oRect = oRange.getBoundingClientRect();
+//   // console.log(oRect)
+//   // console.log(oRect['x'])
+//   // console.log(oRect['y'])
+//   dialog.setAttribute(
+//     "style",`
+//     right: 10px;
+//     top: 10px;
+//     bottom: 10px;
+//     height: 90%;
+//     position:absolute;
+//     background-color: white;
+//     border-radius: 6px;
+//     box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+//     color: #4a4a4a;
+//     display: block;
+//     padding: 1.25rem;
+//     `
+//   );
 
-  const iframe = document.getElementById("popup-content");
-  iframe.frameBorder = 0;
-  if(iframe.contentDocument) { 
-    doc = iframe.contentDocument; 
-  } else {
-    doc = iframe.contentWindow.document; 
-  }
+//   const iframe = document.getElementById("popup-content");
+//   iframe.frameBorder = 0;
+//   if(iframe.contentDocument) { 
+//     doc = iframe.contentDocument; 
+//   } else {
+//     doc = iframe.contentWindow.document; 
+//   }
 
-
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
+//   var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
   
-  fetch("https://code.peikai.pii.at/iframe?text="+window.getSelection().toString(), requestOptions)
-    .then(response => {
-          // dialog.showModal()
-          dialog.style.display = "block"
-          return response.text()
-        }
-      )
-    .then(result => {
-      // doc.body.style.fontSize = 0.75 * parseFloat(getComputedStyle(document.documentElement).fontSize);
-      doc.body.innerHTML = "<p style='color: #363636; font-weight: 600;line-height: 1.125; font-size: 1.5em; margin-bottom: 0.5em;'>"+window.getSelection().toString()+"</p>"
-      doc.body.innerHTML += "<p>"+result+"</p>"
-    }
-    )
-    .catch(error => console.log('error', error));
-}
-
-// function showModal2(e) { 
-//   if (e.code == "Backquote"){
-//     if (window.getSelection().toString() == ""){
-//       console.log("there is no selected text")
+//   fetch("https://code.peikai.pii.at/iframe?text="+window.getSelection().toString(), requestOptions)
+//     .then(response => {
+//           // dialog.showModal()
+//           dialog.style.display = "block"
+//           return response.text()
+//         }
+//       )
+//     .then(result => {
+//       // doc.body.style.fontSize = 0.75 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+//       doc.body.innerHTML = "<p style='color: #363636; font-weight: 600;line-height: 1.125; font-size: 1.5em; margin-bottom: 0.5em;'>"+window.getSelection().toString()+"</p>"
+//       doc.body.innerHTML += "<p>"+result+"</p>"
 //     }
-//     else{
-//       console.log("fired")
+//     )
+//     .catch(error => console.log('error', error));
+// }  
 
-//       const dialog = document.querySelector("dialog");
-//       const iframe = document.getElementById("popup-content");
-//       iframe.frameBorder = 0;
-//       if(iframe.contentDocument) { 
-//         doc = iframe.contentDocument; 
-//       } else {
-//         doc = iframe.contentWindow.document; 
-//       }
-
-//       var requestOptions = {
-//         method: 'GET',
-//         redirect: 'follow'
-//       };
-      
-//       fetch("https://code.peikai.pii.at/iframe?text="+window.getSelection().toString(), requestOptions)
-//         .then(response => {
-//               dialog.showModal()
-//               return response.text()
-//             }
-//           )
-//         .then(result => doc.body.innerHTML = result
-//         )
-//         .catch(error => console.log('error', error));
-
-      
-
-      
-//       // content.open();
-//       // content.write("test");
-//       // content.close();
-//     }
+// const closeModal = () => {
+//   modal = document.getElementById("highlightDialog")
+//   if (modal){
+//     modal.style.display = "none";
 //   }
 // }
-  
 
-const closeModal = () => {
-  modal = document.getElementById("highlightDialog")
-  if (modal){
-    modal.style.display = "none";
-  }
-}
-
-document.addEventListener("mouseup", mouseUp);
+// document.addEventListener("mouseup", mouseUp);
 // document.addEventListener("keypress", showModal2);
-document.addEventListener("click", closeModal);
+// document.addEventListener("click", closeModal);
--- cextension/execute.js
+++ cextension/execute.js
@@ -1,4 +1,74 @@
 // this code will be executed when the extension's button is clicked
 (function() {
   console.log('execute.js executed');
+  if (document.getElementById("highlightDialog").style.display == "none"){
+    console.log('open modal')
+    openModal()
+  }
+  else{
+    console.log('close modal')
+    closeModal()
+  }
 })();
+
+function openModal(){
+  const dialog = document.getElementById("highlightDialog");
+  // s = window.getSelection();
+  // oRange = s.getRangeAt(0); //get the text range
+  // oRect = oRange.getBoundingClientRect();
+  // console.log(oRect)
+  // console.log(oRect['x'])
+  // console.log(oRect['y'])
+  dialog.setAttribute(
+    "style",`
+    right: 10px;
+    top: 10px;
+    bottom: 10px;
+    height: 90%;
+    position:absolute;
+    background-color: white;
+    border-radius: 6px;
+    box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1), 0 0px 0 1px rgba(10, 10, 10, 0.02);
+    color: #4a4a4a;
+    display: block;
+    padding: 1.25rem;
+    `
+  );
+
+  const iframe = document.getElementById("popup-content");
+  iframe.frameBorder = 0;
+  if(iframe.contentDocument) { 
+    doc = iframe.contentDocument; 
+  } else {
+    doc = iframe.contentWindow.document; 
+  }
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://code.peikai.pii.at/iframe?text="+window.getSelection().toString(), requestOptions)
+    .then(response => {
+          // dialog.showModal()
+          dialog.style.display = "block"
+          return response.text()
+        }
+      )
+    .then(result => {
+      // doc.body.style.fontSize = 0.75 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+      doc.body.innerHTML = "<p style='color: #363636; font-weight: 600;line-height: 1.125; font-size: 1.5em; margin-bottom: 0.5em;'>"+window.getSelection().toString()+"</p>"
+      doc.body.innerHTML += "<label class='switch'> <input type='checkbox' checked> <span class='slider round'></span></label>"
+      doc.body.innerHTML += "<p>"+result+"</p>"
+      doc.body.innerHTML += "<input type='text' id='fname' name='fname'>"
+    }
+    )
+    .catch(error => console.log('error', error));
+} 
+
+function closeModal() {
+  modal = document.getElementById("highlightDialog")
+  if (modal){
+    modal.style.display = "none";
+  }
+}
